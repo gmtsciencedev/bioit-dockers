149073ca11e3049ffba98cb92f7300af89e28c6d
delahondes <raynald.delahondes@gmt.bio>
2023-01-14 20:22:09 +0100

    add parallel in motus docker

--- motus3/build.sh
+++ motus3/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 DOCKER_NAME=motus
-DOCKER_VERSION=3.0.3
+DOCKER_VERSION=3.0.3.1
 docker build -t gmtscience/${DOCKER_NAME}:${DOCKER_VERSION} .
 docker push gmtscience/${DOCKER_NAME}:${DOCKER_VERSION}
 docker tag gmtscience/${DOCKER_NAME}:${DOCKER_VERSION} gmtscience/${DOCKER_NAME}:latest
